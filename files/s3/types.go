5f2e5ec12cbcd4606902b2330524910646e0389f
mexisme <wildjim+dev@kiwinet.org>
2017-05-12 00:29:09 +1200

    Extract a fair bit of code into sep functions

--- files/s3/types.go
+++ files/s3/types.go
@@ -1,7 +1,6 @@
 package s3
 
 import (
-	"fmt"
 	"net/url"
 	"strings"
 
@@ -13,60 +12,65 @@ import (
 
 // Source Dir for Secret files
 type Source struct {
-	Base   string
-	url    *url.URL
-	bucket string
-	prefix string
+	URLString string
+	URL       *url.URL
+	Bucket    string
+	Prefix    string
 	//session *session.Session
-	s3session *s3.S3
+	S3Session *s3.S3
 }
 
 // Init Source struct
 func (s *Source) Init() *Source {
-	if err := s.initBase(); err != nil {
-		log.Panic(err)
-	}
-	if err := s.initSession(); err != nil {
-		log.Panic(err)
+	if s.Bucket == "" && s.Prefix == "" {
+		if s.URL == nil {
+			if s.URLString == "" {
+				log.Panic("No Base URL provided.")
+			}
+			s.InitURLFromURLString()
+		}
+		s.InitBucketPrefixFromURL()
 	}
 
-	s.bucket = s.url.Host
-	s.prefix = strings.TrimPrefix(s.url.Path, "/") // an initial `/` won't work with S3
+	if s.S3Session == nil {
+		s.InitSessionFromBucketPrefix()
+	}
 
 	return s
 }
 
-func (s *Source) initBase() error {
+func (s *Source) InitURLFromURLString() *Source {
 	var err error
-
-	if s.Base == "" {
-		// FIXME: I really want to combine both of these!
-		log.Error("No URL provided.")
-		return fmt.Errorf("No Base URL provided")
-	}
-	if s.url, err = url.Parse(s.Base); err != nil {
-		log.WithFields(log.Fields{"base-url": s.Base}).Error(err)
-		return err
+	if s.URL, err = url.Parse(s.URLString); err != nil {
+		log.WithFields(log.Fields{"url-string": s.URLString}).Panic(err)
 	}
 
-	if s.url.Scheme != "s3" {
-		log.WithFields(log.Fields{"url": s.url}).Error("Base URL Scheme is not supported.")
-		return fmt.Errorf("Base URL Scheme is not supported: %q", s.url.Scheme)
-	}
-
-	return nil
+	return s
 }
 
-func (s *Source) initSession() error {
+func (s *Source) InitBucketPrefixFromURL() *Source {
+	if s.URL.Scheme != "s3" {
+		log.WithFields(log.Fields{"url": s.URL}).Panic("Base URL Scheme is not supported.")
+	}
+
+	s.Bucket = s.URL.Host
+	// an initial `/` won't work with S3
+	s.Prefix = strings.TrimPrefix(s.URL.Path, "/")
+
+	return s
+}
+
+func (s *Source) InitSessionFromBucketPrefix() *Source {
 	// TODO: Enable AWS_SDK_LOAD_CONFIG env-var, somehow!
 	session, err := session.NewSession()
 	if err != nil {
-		log.Error(err)
-		return err
-
+		log.Panic(err)
 	}
 
-	// TODO: This should be in a config file (or ~/.aws/config):
-	s.s3session = s3.New(session, &aws.Config{Region: aws.String("ap-southeast-2")})
-	return nil
+	// TODO: `Region` should be in a config file (or ~/.aws/config):
+	s.S3Session = s3.New(
+		session,
+		&aws.Config{Region: aws.String("ap-southeast-2")},
+	)
+	return s
 }
--- files/s3/types_test.go
+++ files/s3/types_test.go
@@ -1,6 +1,7 @@
 package s3_test
 
 import (
+	// "net/url"
 	. "build-dotenv/files/s3"
 
 	. "github.com/onsi/ginkgo"
@@ -8,9 +9,25 @@ import (
 )
 
 var _ = Describe("Types", func() {
+	It("", func() {
+		Expect(
+			(&Source{URLString: "s3://a/b"}).InitURLFromURLString().URL.Host).To(Equal("a"))
+	})
+
+	It("Good Base URL (no session)", func() {
+		s3ish := (&Source{URLString: "s3://a/b"}).InitURLFromURLString().InitBucketPrefixFromURL()
+
+		Expect(s3ish.Bucket).To(Equal("a"))
+		Expect(s3ish.Prefix).To(Equal("b"))
+	})
+
 	It("Bad Base URL Panics", func() {
 		Expect(func() {
-			(&Source{Base: "no-way"}).Init()
+			(&Source{URLString: "no-way"}).Init()
 		}).To(Panic())
+	})
+
+	It("Bad Session Panics", func() {
+		Skip("Have to mock / fake the AWS SDK?")
 	})
 })
