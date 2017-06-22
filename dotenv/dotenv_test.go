933e37c50fc3f7ea583573c63f0883c16ff33ed8
mexisme <wildjim+dev@kiwinet.org>
2017-06-22 23:36:45 +1200

    Test placeholder for dotenv package

---
+++ dotenv/dotenv_test.go
@@ -0,0 +1,16 @@
+package dotenv_test
+
+import (
+	// TODO: Add this back when some tests are written:
+	// . "build-dotenv/dotenv"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Dotenv", func() {
+	It("", func() {
+		Skip("Placeholder")
+		Expect(true).To(BeTrue())
+	})
+})
