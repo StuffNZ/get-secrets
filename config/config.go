aa1606ae75108331f7a784c4bc48cc5190a02b76
mexisme <wildjim+dev@kiwinet.org>
2017-06-22 20:43:31 +1200

    Moved the config.go into its own package

--- config/config.go
+++ config/config.go
@@ -1,10 +1,16 @@
-package main
+package config
 
 import (
 	"github.com/evalphobia/logrus_sentry"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"os"
+	"time"
+)
+
+var (
+	initConfigDone = false
+	logConfigDone  = false
 )
 
 func init() {
@@ -12,7 +18,25 @@ func init() {
 	configLogging()
 }
 
+// ImportMe is to allow other packages to easily depend on this one,
+// since most of the important logic is in init()
+func ImportMe() {
+}
+
+// AddConfigItems does
+func AddConfigItems(configItems []string) {
+	readConfig()
+	for _, item := range configItems {
+		viper.BindEnv(item)
+	}
+}
+
 func readConfig() {
+	// This should make it safe to rerun a few times
+	if initConfigDone {
+		return
+	}
+
 	viper.SetEnvPrefix("secrets")
 	viper.BindEnv("debug")
 	viper.BindEnv("base")
@@ -32,15 +56,22 @@ func readConfig() {
 		// panic(fmt.Errorf("Fatal error config file: %s \n", err))
 		log.WithFields(log.Fields{"config_file": viper.ConfigFileUsed()}).Warn(err)
 	}
+
+	initConfigDone = true
 }
 
 func configLogging() {
+	// This should make it safe to rerun a few times
+	if logConfigDone {
+		return
+	}
 	if viper.GetBool("debug") {
 		log.SetLevel(log.DebugLevel)
 		log.Debug("Debug mode enabled")
 	}
 
 	if sentryDsn := viper.GetString("sentry.dsn"); sentryDsn != "" {
+		log.WithFields(log.Fields{"sentry.dsn": sentryDsn}).Debug("Configuring connection to Sentry.io")
 		tags := map[string]string{
 			"app": viper.GetString("app"),
 		}
@@ -59,9 +90,11 @@ func configLogging() {
 				}
 			}
 			hook.StacktraceConfiguration.Enable = true
+			// It seems as if the default 100ms is too short:
+			hook.Timeout = 1 * time.Second
 			log.AddHook(hook)
 
-			log.WithFields(log.Fields{"sentry.dsn": sentryDsn}).Debug("Sentry enabled")
+			log.Debug("Sentry enabled")
 
 		} else {
 			log.Warn(err)
@@ -69,4 +102,6 @@ func configLogging() {
 	}
 
 	// viper.Debug()
+
+	logConfigDone = true
 }
